de10351b8e49bce12d3e80dacacf394a81bdbdf7
ijazahmad-star <ijaz.ahmad@strategisthub.com>
2025-12-11 17:48:01 +0500

    fix:

--- scripts/create-admin.ts
+++ scripts/create-admin.ts
@@ -3,11 +3,11 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 import bcrypt from "bcryptjs";
-const DATABASE_URL="postgresql://postgres.xzsrgynkhhptqrorolmr:Ijaz@123-ahmad@aws-1-ap-southeast-2.pooler.supabase.com:5432/postgres"
+
 async function createAdminUser() {
   try {
     // Create connection directly
-    const connectionString = DATABASE_URL;
+    const connectionString = process.env.DATABASE_URL;
     if (!connectionString) {
       throw new Error("DATABASE_URL is not set");
     }
